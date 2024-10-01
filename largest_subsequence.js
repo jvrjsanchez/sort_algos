fda60aaf0897fb1f8f1d272c07981aa24f619cc2
jvrjsanchez <javiersanchez.cna@gmail.com>
2024-09-30 22:03:59 -0500

    medium leetcode

--- largest_subsequence.js
+++ largest_subsequence.js
@@ -1,21 +1,21 @@
-class Solution{
-    LongestRepeatingSubsequence(str){
-        var n = str.length
-        var dp = new Array(n+1);
-        for (var i = 0; i <= n; i++){
-            dp[i] = new Array(n+1)
-            for (var j = 0; j <= n; j++){
-                dp[i][j] = 0;
+var longestStrChain = function(words) {
+        words.sort((a,b) => a.length - b.length)
+        const dp = new Map()
+        let longestChain = 1
+        for (const word of words){
+            dp.set(word, 1)
+        }
+        for (const word of words){
+            for(var i = 0; i < word.length; i++){
+                const predecessor = word.slice(0, i) + word.slice(i + 1)
+                if (dp.has(predecessor)){
+                    dp.set(word, Math.max(dp.get(word), dp.get(predecessor) + 1))
+                }
+                longestChain = Math.max(longestChain, dp.get(word))
             }
         }
-        for (i = 1; i <= n; i++){
-            for (j = 1; j <= n; j++){
-                if ((str(i - 1) === str(j - 1) && i != j)
-                    dp[i][j] = 1 + dp[i-1][j-1]
-                else
-                    dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j])
-            }
-        }
-        return dp[n][n]
+        return longestChain;
     }
-}
+// Example usage:
+const words = ["abcd","dbqca"];
+console.log(longestStrChain(words)); // Output: 5
