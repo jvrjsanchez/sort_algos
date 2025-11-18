ef89ef1a2278c0ac794ada0171cbe351233ed3b0
jvrjsanchez <javiersanchez.cna@gmail.com>
2025-11-18 16:11:23 -0600

    RAG_part1

---
+++ 22-Cap18/dsa_env_var.py
@@ -0,0 +1,9 @@
+# Projeto 9 - IA Generativa e RAG Para App de Sistema Inteligente de Busca em Documentos - Backend e API
+# Módulo da API Para o LLM
+
+# Import
+import os
+
+# Nvidia API
+nvidia_key = "coloque-aqui-sua-api"
+
